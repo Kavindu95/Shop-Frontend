cc2d12f787fb01a63a9e58ab53b814be870a4fa7
Kavindu95 <kavindueng95@gmail.com>
2024-03-16 22:25:10 +0530

    adjestment

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-} from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import POSPage from './pages/POSPage';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-import InsertProduct from './pages/InsertProduct';
+  BrowserRouter,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import POSPage from "./pages/POSPage";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import InsertProduct from "./pages/InsertProduct";
 
 function App() {
   return (
-    <Router>
-    <Routes>
-      <Route index path="/" element={<HomePage/>} />
-      <Route path="/pos" element={<POSPage/>} />
-      <Route path="/signup" element={<SignUp/>} />
-      <Route path="/login" element={<SignIn/>} />
-      <Route path="/product" element={<InsertProduct/>} />
-
-    </Routes>
-  </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/pos" element={<POSPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/product" element={<InsertProduct />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
